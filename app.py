c9ab303c565c95c861389aa1140861a1b4c9dea9
Haroon Afridi <118681059+haroon427666@users.noreply.github.com>
2026-02-21 19:11:29 +0500

    Implement model download from Google Drive
    Added functionality to download the model from Google Drive if it does not exist locally.

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
+import os
 import streamlit as st
+import gdown
 from PIL import Image
 from model import load_model, predict_image
-import torch
 
-MODEL_PATH = "C:\Users\-\Desktop\.ipynb_checkpoints\Machine_learning\APPS\ai_vs_real_app\checkpoints\best_model.pth"
+MODEL_PATH = "best_model.pth"
+FILE_ID = "1pk7siHLeWZaiafyVviSR3nNAS4gJ5qGh"
+GDRIVE_URL = f"https://drive.google.com/uc?id={FILE_ID}"
 
-st.set_page_config(page_title="AI vs Real Detector", layout="centered")
+# Download model if needed,
+if not os.path.exists(MODEL_PATH):
+    with st.spinner("⬇️ Downloading model..."):
+        gdown.download(GDRIVE_URL, MODEL_PATH, quiet=False)
+
+st.set_page_config(page_title="AI vs Real Detector")
 
 st.title("🧠 AI vs Real Image Classifier")
-st.write("Upload an image and check if it is AI-generated or Real.")
 
-# Load model once
 @st.cache_resource
 def get_model():
     return load_model(MODEL_PATH)
 
 model = get_model()
 
-uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png", "webp"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg", "webp"])
 
 if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     if st.button("Predict"):
-        with st.spinner("Analyzing..."):
+        with st.spinner("🔍 Predicting..."):
             pred, conf = predict_image(model, image)
 
             if pred == 1:
-                label = "🤖 AI Generated"
+                st.success(f"🤖 AI Generated ({conf*100:.2f}%)")
             else:
-                label = "📷 Real Image"
-
-            st.success(f"Prediction: {label}")
-            st.info(f"Confidence: {conf*100:.2f}%")
+                st.success(f"📷 Real Image ({conf*100:.2f}%)")
